906242e3c15c38626d56f5884bc863d6ac02545b
yppeanuts7 <yp.peanuts7@icloud.com>
2025-07-27 10:07:14 +0900

    最新の変更をコミット

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /** Next.jsはサーバ側で動的にページを作るフレームワークなのでGitHub Pagesのような静的ホスティングサービスでは基本的に動作しない */
-  /** Next.jsを静的サイトにビルドしてそれをGitHub Pagesにアップロードする */
   output: 'export',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
